bdd99f3b5ace7d5be86832a1b48cc7a9860ac804
yangwenchao <jd_yangwenchao@163.com>
2020-06-29 17:53:58 +0800

    文件上传

---
+++ src/main/java/com/chao/admin/fileTools/config/FileUploadConfig.java
@@ -0,0 +1,21 @@
+package com.chao.admin.fileTools.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class FileUploadConfig {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement(){
+
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //上传文件大小限制 10MB
+        factory.setMaxFileSize(DataSize.ofBytes(1048576000));
+        return factory.createMultipartConfig();
+    }
+}
--- src/main/java/com/chao/admin/login/controller/LoginController.java
+++ src/main/java/com/chao/admin/login/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.chao.admin.login.controller;
 
 import com.chao.admin.login.service.LoginService;
 import com.chao.admin.result.Result;
+import com.chao.admin.result.ResultCode;
 import com.chao.admin.vo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -9,6 +10,11 @@ import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.UUID;
 
 /**
  * 登录控制
@@ -39,6 +45,90 @@ public class LoginController {
 
         Result result = loginService.login(user, headers);
         return result;
+    }
+
+    /**
+     * 用户头像上传
+     * @param file
+     * @author 杨文超
+     * @date 2020-06-29
+     */
+    @ApiImplicitParams({
+
+            @ApiImplicitParam(name = "Accept",value = "",required = false,dataType = "String",paramType = "header",defaultValue = "application/json")
+    })
+    @PostMapping(value = "/upload")
+    public Result login(@RequestParam("file")MultipartFile file){
+        if(file.isEmpty()){
+            return new Result(ResultCode.businErrorCode.getCode(),"未上传头像");
+        }
+        String fileName = UUID.randomUUID().toString();
+        String originalFilename = file.getOriginalFilename();
+        String[] split = originalFilename.split(".");
+        System.out.println(split);
+        String s = split[split.length];
+        String path = "D:/test" ;
+        File dest = new File(path + "/" + fileName+"."+s);
+        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); //保存文件
+            return new Result(ResultCode.successCode.getCode(),ResultCode.successCode.getMsg());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(ResultCode.businErrorCode.getCode(),"上传头像失败",e.getMessage());
+        }
+    }
+
+    /**
+     * 用户头像下载
+     * @author 杨文超
+     * @date 2020-06-29
+     */
+    @ApiImplicitParams({
+
+            @ApiImplicitParam(name = "Accept",value = "",required = false,dataType = "String",paramType = "header",defaultValue = "application/json")
+    })
+    @RequestMapping("/download")
+    public String downLoad(HttpServletResponse response) throws UnsupportedEncodingException {
+        String filename="2.xlsx";
+        String filePath = "D:/download";
+        File file = new File(filePath + "/" + filename);
+        if(file.exists()){ //判断文件父目录是否存在
+            response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            // response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" +   java.net.URLEncoder.encode(filename,"UTF-8"));
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("----------file download---" + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
     /**
