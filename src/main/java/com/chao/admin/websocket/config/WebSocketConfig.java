35408c00b5042a0468f2ee76d337eb63ea80a37a
yangwenchao <jd_yangwenchao@163.com>
2020-07-16 10:04:47 +0800

    websocket

---
+++ src/main/java/com/chao/admin/websocket/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.chao.admin.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ * @author 杨文超
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
